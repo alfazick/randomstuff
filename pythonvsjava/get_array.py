8677170d8dcbfe731b90ef405516530d3f12eec2
Askar Nurbekov <nurbekoff@gmail.com>
2024-03-24 20:25:38 +0000

    python versus java

---
+++ pythonvsjava/get_array.py
@@ -0,0 +1,4 @@
+import numpy as np 
+def get():
+    arr = np.arange(1,5);
+    return arr;
---
+++ pythonvsjava/hello.py
@@ -0,0 +1,6 @@
+
+
+import get_array
+arr = get_array.get()
+
+print("Hello, World: {}".format(repr(arr)))
